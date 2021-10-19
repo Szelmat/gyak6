11afa6d2da007d47ecc9509477046c9d784ca570
Széles Máté <szelessm@gmail.com>
2021-10-20 00:05:03 +0200

    callback has been called twice

--- test.js
+++ test.js
@@ -1,6 +1,14 @@
+import axios from "axios";
 import { forEach, sum } from "./main";
+import { Users } from "./users";
 
 describe("Testing mock functions", () => { 
   let mockCallback = jest.fn((x) => 42 + x);
   forEach([0, 1], mockCallback);
+
+  describe("Testing mock function cration", () => {
+    test("The mock function is called twice", () => {
+      expect(mockCallback.mock.calls.length).toBe(2);
+    });
+  });
 });

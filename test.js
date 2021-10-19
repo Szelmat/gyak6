de71241f26d987a71b00fb0d058e08e6efedda0f
Széles Máté <szelessm@gmail.com>
2021-10-20 00:11:05 +0200

    testing callback parameters

--- test.js
+++ test.js
@@ -6,7 +6,15 @@ describe("Testing mock functions", () => {
 
   describe("Testing mock function cration", () => {
     test("The mock function is called twice", () => {
-      expect(mockCallback.mock.calls.length).toBe(2);
+      expect(mockCallback.mock.calls.length).toBe(3);
+    });
+
+    test("The first argument of the first function call was 0", () => {
+      expect(mockCallback.mock.calls[0][0]).toBe(0);
+    });
+
+    test("The first argument of the second call to the function was 1", () => {
+      expect(mockCallback.mock.calls[1][0]).toBe(1);
     });
   });
 });
